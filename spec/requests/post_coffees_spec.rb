e0e69696d8201c8e18b28d871092d47011bf7814
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-29 14:38:07 -0700

    Create spec to test 422 error in post request.
    Co-authored-by: David Couch <dcouch440@gmail.com>
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- spec/requests/post_coffees_spec.rb
+++ spec/requests/post_coffees_spec.rb
@@ -1,22 +1,33 @@
 require 'rails_helper'
 
 describe 'post a coffee route', :type => :request do
-  before do
-    post '/coffees', params: { blend_name: 'Test Blend', origin: 'Test Origin', notes: 'Mmm, mmm, good'}
-  end
-  it 'returns the blend name' do
-    expect(JSON.parse(response.body)["coffee"]['blend_name']).to eq('Test Blend')
+  context 'when successful' do
+    before do
+      post '/coffees', params: { blend_name: 'Test Blend', origin: 'Test Origin', notes: 'Mmm, mmm, good'}
+    end
+    it 'returns the blend name' do
+      expect(JSON.parse(response.body)["coffee"]['blend_name']).to eq('Test Blend')
+    end
+
+    it 'returns the origin' do
+      expect(JSON.parse(response.body)["coffee"]['origin']).to eq('Test Origin')
+    end
+
+    it 'returns the notes' do
+      expect(JSON.parse(response.body)["coffee"]['notes']).to eq('Mmm, mmm, good')
+    end
+
+    it 'returns a created status' do
+      expect(response).to have_http_status(201)
+    end
   end
 
-  it 'returns the origin' do
-    expect(JSON.parse(response.body)["coffee"]['origin']).to eq('Test Origin')
-  end
-
-  it 'returns the notes' do
-    expect(JSON.parse(response.body)["coffee"]['notes']).to eq('Mmm, mmm, good')
-  end
-
-  it 'returns a created status' do
-    expect(response).to have_http_status(201)
+  context 'when not successful' do
+    before do
+      post '/coffees', params: { blend_name: '', origin: 'Test Origin', notes: 'Mmm, mmm, good'}
+    end
+    it 'returns an unproccesable entity status' do
+      expect(response).to have_http_status(422)
+    end
   end
 end
