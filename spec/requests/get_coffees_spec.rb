39a87ca1846c1a98b5dbff99edf40380ce6f61c6
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-29 14:07:02 -0700

    Create specs for coffee post request.
    Co-authored-by: David Couch <dcouch440@gmail.com>
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ spec/requests/get_coffees_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe "the coffees get requests" do
+  let!(:coffees) { FactoryBot.create_list(:coffee, 20) }
+
+  describe 'GET /coffees', :type => :request do
+    context 'when successful' do
+      before { get "/coffees" }
+
+      it { expect(response).to have_http_status(200) }
+
+      it 'returns all coffees' do
+        expect(JSON.parse(response.body).count).to eq(coffees.count)
+      end
+    end
+  end
+
+  describe 'GET /coffees/:id', :type => :request do
+    context 'when successful' do
+      before { get "/coffees/#{coffees.first.id}" }
+
+      it { expect(response).to have_http_status(200) }
+
+      it 'returns a coffee based on id' do
+        expect(JSON.parse(response.body)['blend_name']).to eq(coffees.first.blend_name)
+      end
+    end
+
+    context 'when not successful' do
+      before { get "/coffees/#{ coffees.last.id + 1 }" }
+      it { expect(response).to have_http_status(404) }
+    end
+  end
+end
---
+++ spec/requests/post_coffees_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'post a coffee route', :type => :request do
+  before do
+    post '/coffees', params: { blend_name: 'Test Blend', origin: 'Test Origin', notes: 'Mmm, mmm, good'}
+  end
+  it 'returns the blend name' do
+    expect(JSON.parse(response.body)["coffee"]['blend_name']).to eq('Test Blend')
+  end
+
+  it 'returns the origin' do
+    expect(JSON.parse(response.body)["coffee"]['origin']).to eq('Test Origin')
+  end
+
+  it 'returns the notes' do
+    expect(JSON.parse(response.body)["coffee"]['notes']).to eq('Mmm, mmm, good')
+  end
+
+  it 'returns a created status' do
+    expect(response).to have_http_status(201)
+  end
+end
