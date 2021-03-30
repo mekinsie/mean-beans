dbb283435bf5e1024ea4343025d058c8bbd457eb
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-30 09:25:55 -0700

    Create user model and migration.
    Co-authored-by: David Couch <dcouch440@gmail.com>
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  def generate_api_key
+    loop do
+      token = SecureRandom.base64.tr('+/=', 'Qrt')
+      break token unless User.exists?(api_key: token).any?
+    end
+  end
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
+  factory :user do
+    api_key { "MyString" }
+  end
+
   factory(:coffee) do
     blend_name {Faker::Coffee.blend_name}
     origin {Faker::Coffee.origin}
